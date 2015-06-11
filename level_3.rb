8e02f12c4580ec085b51c6f6579ead4411309496
Victoria <vikshab2010@hotmail.com>
2015-06-10 20:30:45 -0700

    Changed code for level 3

--- level_3.rb
+++ level_3.rb
@@ -1,15 +1,3 @@
-#   def play_turn(warrior)
-#     if warrior.feel.empty? && !poor_health?(warrior)
-#        walk(warrior)
-#     elsif poor_health?(warrior) && warrior.feel.empty?
-#         gain_health(warrior)
-#     else
-#       attack(warrior)
-#     end
-#   end
-# end
-
-
 class Player
   def play_turn(warrior)
     if warrior.feel.empty? && !(warrior.health < 10)
