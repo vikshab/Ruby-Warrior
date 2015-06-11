010a75190db8d13024a8de23f43b90dd43633e59
Victoria <vikshab2010@hotmail.com>
2015-06-10 20:23:10 -0700

    Complete level 4

---
+++ level_4.rb
@@ -0,0 +1,25 @@
+class Player
+
+  def attacked?
+    @warrior.health < @health
+  end
+
+ def play_turn(warrior)
+   @warrior = warrior
+   # if health exists it will not create
+   @health  ||= 20
+
+  if @warrior.feel.empty? && attacked?
+     @warrior.walk!
+  elsif !(@warrior.feel.empty?) #&& attacked?
+     @warrior.attack!
+  elsif !attacked? && @warrior.feel.empty? && @warrior.health >= 15
+      @warrior.walk!
+  elsif  !attacked? && !(@warrior.feel.empty?) && @warrior.health >= 15
+      @warrior.attack!
+  elsif !attacked? && @warrior.health < 15
+      @warrior.rest!
+  end
+   @health = @warrior.health
+ end
+end
